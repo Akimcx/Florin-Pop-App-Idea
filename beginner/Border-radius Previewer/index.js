81bfe1f8cdf46f76482ff627f6d93f40f6c37ece
Akim-dev <silverdleo@protonmail.com>
2020-09-27 09:34:19 -0300

    New verion, new design 🥰

--- beginner/Border-radius Previewer/index.js
+++ beginner/Border-radius Previewer/index.js
@@ -1,34 +1,59 @@
-const topLeft = document.getElementById("topLeft")
-const topRight = document.getElementById("topRight")
-const bottomLeft = document.getElementById("bottomLeft")
-const bottomRight = document.getElementById("bottomRight")
-const inputElts = document.querySelectorAll("input")
+const inputElts = document.querySelectorAll("input[type=number]")
+const ranges = document.querySelectorAll("range")
+const allBorder = document.getElementById("all-border")
+const abNumber = document.getElementById("ab-number")
+
+
+
+allBorder.addEventListener("input", e => {
+    changeAll(e.target.value)
+})
+
+abNumber.addEventListener("input", e => {
+    changeAll(e.target.value)
+})
+
+
+
 
 let prev
 
-
 inputElts.forEach(input => {
     input.addEventListener("input", e => {
-        const data = e.data
-
-        if(!data) return
-
-        if(isLetter(data)) {
-            input.value = prev
-            return
-        }
-
-        updateCss(input.id, input.value)
-
-        prev = input.value 
+        changeOne(e.data, input)
+        
     })
 })
+
+function changeOne(data, input) {
+    if(!data) return
+
+    if(isLetter(data)) {
+        input.value = prev
+        return
+    }
+
+
+    updateCss(input.id, input.value)
+
+    prev = input.value 
+}
+
+function changeAll(value) {
+    abNumber.value = value
+
+    const box = document.getElementById("box")
+
+    box.style.setProperty(`--topLeft`, `${value}px`)
+    box.style.setProperty(`--topRight`, `${value}px`)
+    box.style.setProperty(`--bottomLeft`, `${value}px`)
+    box.style.setProperty(`--bottomRight`, `${value}px`)
+}
 
 function updateCss(variable,value){
     const box = document.getElementById("box")
 
     box.style.setProperty(`--${variable}`, `${value}px`)
-    updateBoxText()
 }
 
 function isLetter(character){
@@ -39,11 +64,10 @@ window.onload = ()=> {
     inputElts.forEach(input => {
         input.value = ""
     })
-}
 
-function updateBoxText() {
-    const cssBox = document.getElementById("css-box")
+    ranges.forEach(range => {
+        range.value = 0
+    })
 
-    cssBox.innerText = `
-        border-radius: ${topLeft.value}px ${topRight.value}px ${bottomRight.value}px ${bottomLeft.value}px`
+    allBorder.value = 0
 }
